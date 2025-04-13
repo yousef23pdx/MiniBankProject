3f5916eece00003f490da3382e989fe6c5f9a2f1
Yousef Alothman <yousefoalothman@gmail.com>
2025-04-13 17:29:46 +0300

    Initializing the Project using spring boot initializer.

---
+++ src/main/kotlin/com/example/Mini_Bank_Project/MiniBankProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Mini_Bank_Project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniBankProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniBankProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Mini_Bank_Project/MiniBankProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Mini_Bank_Project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniBankProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
